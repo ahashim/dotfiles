c2eb357caffbe03b5f726f7a56dbecf0e9ef8340
Ahmed Hashim <ahashim@users.noreply.github.com>
2023-03-08 11:19:56 -0500

    nvim: `<Esc>` -> `<esc>` to clear search highlight

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,7 +2,7 @@ local map = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- General
-map.set("n", "<Esc>", ":noh <CR>", opts) -- clear search highlight
+map.set("n", "<esc>", ":noh <CR>", opts) -- clear search highlight
 map.set("n", "<leader>w", ":write <CR>", opts) -- quick save
 map.set("n", "<leader>q", ":quit! <CR>", opts) -- quick close
 map.set("n", "<leader>x", ":exit <CR>", opts) -- quick save & close
