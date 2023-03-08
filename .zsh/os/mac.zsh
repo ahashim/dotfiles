6ce7b3038438617d135161819b4d47c8043740d4
Ahmed Hashim <ahashim@users.noreply.github.com>
2023-03-08 09:14:22 -0500

    zim: moving zstyle out of mac zsh loader and into zimrc

--- .zsh/os/mac.zsh
+++ .zsh/os/mac.zsh
@@ -1,9 +1,6 @@
 # Environment Variables
 export ARCHFLAGS="-arch arm64"
 
-# zim plugin for homebrew aliases
-zmodule homebrew
-
 # Set PATH, MANPATH, etc., for Homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
