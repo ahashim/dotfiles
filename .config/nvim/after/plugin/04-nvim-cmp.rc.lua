2d7a68d6f808d232ddf29d354ed5c4eee5ef801d
Ahmed Hashim <ahashim@users.noreply.github.com>
2023-05-06 12:02:36 -0700

    nvim: setting priority weight for cmp with copilot

--- .config/nvim/after/plugin/04-nvim-cmp.rc.lua
+++ .config/nvim/after/plugin/04-nvim-cmp.rc.lua
@@ -6,6 +6,14 @@ if not status then
     return
 end
 
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
+end
+
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -36,7 +44,9 @@ cmp.setup {
             select = false,
         },
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
+            if cmp.visible() and has_words_before() then
+                cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+            elseif cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
@@ -53,6 +63,24 @@ cmp.setup {
                 fallback()
             end
         end, { "i", "s" }),
+    },
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            require("copilot_cmp.comparators").prioritize,
+
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
     },
     sources = {
         { name = "copilot" },
