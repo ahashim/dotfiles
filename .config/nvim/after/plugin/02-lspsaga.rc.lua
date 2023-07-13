1d6cee44020f7d59b1916661c742f7fa21640ea0
Ahmed Hashim <ahashim@users.noreply.github.com>
2023-07-13 13:58:00 -0700

    nvim: updating Lspsaga finder command

--- .config/nvim/after/plugin/02-lspsaga.rc.lua
+++ .config/nvim/after/plugin/02-lspsaga.rc.lua
@@ -140,7 +140,7 @@ local opts = { noremap = true, silent = true }
 -- If there is no definition, it will instead be hidden
 -- When you use an action in finder like "open vsplit",
 -- you can use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+keymap("n", "gh", "<cmd>Lspsaga finder<CR>", opts)
 
 -- Code action
 keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
