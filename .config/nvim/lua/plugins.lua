532b89a434f6b3356f96157276b6aef7df7201a2
Ahmed Hashim <ahashim@users.noreply.github.com>
2023-11-06 15:46:54 -0800

    nvim: updating telescope.nvim to always update from latest release tag

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,97 +1,97 @@
 local status, packer = pcall(require, "packer")
 if not status then
-  print "Packer is not installed"
-  return
+    print "Packer is not installed"
+    return
 end
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 packer.init {
-  auto_clean = true,
-  compile_on_sync = true,
-  display = {
-    open_fn = function()
-      return require("packer.util").float {
-        border = "single",
-      }
-    end,
-    prompt_border = "single",
-  },
-  git = {
-    clone_timeout = 6000,     -- seconds
-  },
+    auto_clean = true,
+    compile_on_sync = true,
+    display = {
+        open_fn = function()
+            return require("packer.util").float {
+                border = "single",
+            }
+        end,
+        prompt_border = "single",
+    },
+    git = {
+        clone_timeout = 6000, -- seconds
+    },
 }
 --
 
 return require("packer").startup(function(use)
-  use {
-    "akinsho/bufferline.nvim",
-    tag = "v2.*",
-  }
-  use "folke/trouble.nvim"
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-path"
-  use {
-    "iamcco/markdown-preview.nvim",
-    run = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  }
-  use "j-hui/fidget.nvim"
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "nvim-tree/nvim-tree.lua"
-  use "nvim-tree/nvim-web-devicons"
-  use "nvimdev/dashboard-nvim"
-  use "nvimdev/lspsaga.nvim"
-  use "L3MON4D3/LuaSnip"
-  use "lewis6991/gitsigns.nvim"
-  use "lewis6991/impatient.nvim"
-  use "lukas-reineke/indent-blankline.nvim"
-  use "mfussenegger/nvim-dap"
-  use "navarasu/onedark.nvim"
-  use "norcalli/nvim-colorizer.lua"
-  use "numToStr/Comment.nvim"
-  use "neovim/nvim-lspconfig"
-  use "nvim-lua/plenary.nvim"
-  use "nvim-lualine/lualine.nvim"
-  use {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
-  }
-  use "nvim-telescope/telescope-fzf-native.nvim"
-  use "nvim-telescope/telescope-media-files.nvim"
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-  use "ojroques/nvim-bufdel"
-  use "rafamadriz/friendly-snippets"
-  use "saadparwaiz1/cmp_luasnip"
-  use "simrat39/rust-tools.nvim"
-  use "TovarishFin/vim-solidity"
-  use "tpope/vim-fugitive"
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  use "wbthomason/packer.nvim"
-  use "windwp/nvim-autopairs"
-  use "windwp/nvim-ts-autotag"
-  use "zbirenbaum/copilot.lua"
-  use "zbirenbaum/copilot-cmp"
+    use {
+        "akinsho/bufferline.nvim",
+        tag = "v2.*",
+    }
+    use "folke/trouble.nvim"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lua"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-path"
+    use {
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    }
+    use "j-hui/fidget.nvim"
+    use "jose-elias-alvarez/null-ls.nvim"
+    use "nvim-tree/nvim-tree.lua"
+    use "nvim-tree/nvim-web-devicons"
+    use "nvimdev/dashboard-nvim"
+    use "nvimdev/lspsaga.nvim"
+    use "L3MON4D3/LuaSnip"
+    use "lewis6991/gitsigns.nvim"
+    use "lewis6991/impatient.nvim"
+    use "lukas-reineke/indent-blankline.nvim"
+    use "mfussenegger/nvim-dap"
+    use "navarasu/onedark.nvim"
+    use "norcalli/nvim-colorizer.lua"
+    use "numToStr/Comment.nvim"
+    use "neovim/nvim-lspconfig"
+    use "nvim-lua/plenary.nvim"
+    use "nvim-lualine/lualine.nvim"
+    use {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.*",
+    }
+    use "nvim-telescope/telescope-fzf-native.nvim"
+    use "nvim-telescope/telescope-media-files.nvim"
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+    use "ojroques/nvim-bufdel"
+    use "rafamadriz/friendly-snippets"
+    use "saadparwaiz1/cmp_luasnip"
+    use "simrat39/rust-tools.nvim"
+    use "TovarishFin/vim-solidity"
+    use "tpope/vim-fugitive"
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+    use "wbthomason/packer.nvim"
+    use "windwp/nvim-autopairs"
+    use "windwp/nvim-ts-autotag"
+    use "zbirenbaum/copilot.lua"
+    use "zbirenbaum/copilot-cmp"
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
